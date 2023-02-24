bd82710b813b58e00e4a2db111ea8c41a1e6a5a4
Kelan Mazey <kelanmazey@gmail.com>
2023-02-24 20:10:59 +0000

    adds postgres and adminer container to docker compose

--- get_demos/get_demos.py
+++ get_demos/get_demos.py
@@ -1,41 +1,58 @@
 import requests
+import argparse
 from bs4 import BeautifulSoup
-from types import List
 
 
-def main():
-  # Get HTML doc for beautiful soup
-  # Hardcoding initial offset as this is as far back as matches go, getting future demos can be from daily run of "hltv.org/results"
-  # Do I want to add an offset of 1 to wait for demos?
-  urls = get_urls()
-
-  # TODO: Get list of HLTV match IDs + team names + competition
+def main(args):
+  # Get match URLS
+  match_urls = get_match_urls(args)
 
   # TODO: Check URL to see if demo
+  check_demos = get_demo_urls(match_urls)
 
-  # TODO: Save data: HLTV_match_URL, HLTV_match_id, team_a, team_b, competition, demo_available, date
+  # TODO: Save data: HLTV_match_URL, HLTV_match_id, team_a, team_b, competition, demo_url, date
+  # load_data_to_db()
 
-
-def get_urls(**kwargs) -> List[str]:
-  offset = kwargs.get("offset", 0)
-
+def get_match_urls(args) -> list[str]:
+  # Get HTML doc for beautiful soup
+  offset = args.offset
   results_page = requests.get(
-    url=f"https://www.hltv.org/results/offset={offset}",
+    url=f"https://www.hltv.org/results?offset={offset}",
     headers={
       "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
       "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     })
   
   hltv_results = BeautifulSoup(results_page.text, 'html.parser')
-  results =[]
+  raw_results =[]
 
   # Pull match link from html
   for a in hltv_results.find_all("a", class_="a-reset"):
     link = str(a.get("href"))
     
     if link.startswith("/matches/"):
-      results.append(link)
+      raw_results.append(link)
+  
+  # This is to remove any duplicates added from the 'Featured results' pane on most recent matches
+  # NOTE: I tried to use bs4 to search for the results-all class but couldn't get it to return  
+  results = []
+  for match in raw_results:
+    if match not in results:
+      results.append(match)
+
+  return results
+
+def get_demo_urls(match_urls: list[str]) -> tuple[str, str, str]:
+  pass
+
+def load_data_to_db(db_connection, **kwargs):
+  pass
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Scrapes HLTV.org and loads results to a postgres DB")
+  parser.add_argument("--offset", type=str, default=0, help="Number of matches to offset for HLTV results page, default is 0\nThis argument must be used with '=' ie. --offset=100")
 
-  main()
+  parser.add_argument("--date", type=str, help="Arg must be used with '=' ie. --date=\"25-12-2023\", no default")
+
+  args = parser.parse_args()
+  main(args)
