71a68671c19143cd61381cdfc45d28723cdf03e9
Kelan Mazey <kelanmazey@gmail.com>
2023-04-02 16:11:34 +0100

    adds scripts dependency on db

--- get_demos/get_demos.py
+++ get_demos/get_demos.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 from bs4 import BeautifulSoup
 
+HLTV_ADDR = 'https://www.hltv.org/'
 
 def main(args):
   offset = int(args.offset)
@@ -29,7 +30,7 @@ def main(args):
 def get_match_urls(offset) -> list[str]:
   # Get HTML doc for beautiful soup
   results_page = requests.get(
-    url=f"https://www.hltv.org/results?offset={offset}",
+    url=f"{HLTV_ADDR}results?offset={offset}",
     headers={
       "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
       "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
@@ -48,9 +49,13 @@ def get_match_urls(offset) -> list[str]:
   return results
 
 def get_demo_urls(match_urls: list[str]) -> tuple[str, str, str]:
+  # TODO: Go to match page
+  # TODO: Search for demo link
+  # TODO: pair demos with match
   pass
 
 def load_data_to_db(db_connection, **kwargs):
+
   pass
 
 if __name__ == "__main__":
