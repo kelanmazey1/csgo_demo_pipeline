209d36257552443d7e98678eb3012607ee389464
Kelan Mazey <kelanmazey@gmail.com>
2023-04-02 12:42:14 +0100

    updates docker compose to use db secrets and create db table with init

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE csgo_parser_data OWNER secrets.user;
-
-CREATE TABLE IF NOT EXISTS hltv_metadata (
+CREATE SCHEMA IF NOT EXISTS hltv;
+CREATE TABLE IF NOT EXISTS hltv.hltv_metadata (
   match_id smallint PRIMARY KEY,
   team_a varchar(40),
   team_b varchar(40),
@@ -9,5 +8,3 @@ CREATE TABLE IF NOT EXISTS hltv_metadata (
   match_url varchar,
   demo_url text
 );
-
-
