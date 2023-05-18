432ce1b9ecf53df27364a4653492eefd9e10a642
Kelan Mazey <kelanmazey@gmail.com>
2023-05-18 18:23:34 +0100

    added new folder for scraper container. Added basic schema for database

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,29 @@
-CREATE SCHEMA IF NOT EXISTS hltv;
-CREATE TABLE IF NOT EXISTS hltv.hltv_metadata (
-  match_id smallint PRIMARY KEY,
+CREATE SCHEMA IF NOT EXISTS staging;
+CREATE TABLE IF NOT EXISTS staging.hltv_metadata (
+  hltv_id INTEGER PRIMARY KEY,
+  team_a_name VARCHAR(40),
+  team_b_name VARCHAR(40),
+  competition VARCHAR(100),
+  "date" DATE,
+  match_url VARCHAR,
+  demo_url TEXT
+);
+
+CREATE TABLE IF NOT EXISTS staging.kills (
+  kill_id SERIAL PRIMARY KEY,
+  match_id INTEGER,
+  hltv_id INTEGER,
+  x_pos INTEGER,
+  y_pos INTEGER,
+  weapon VARCHAR(50),
+  damage SMALLINT
+  CONSTRAINT fk_match
+    FOREIGN KEY(match_id)
+      REFERENCES matches(match_id)
+);
+
+CREATE TABLE IF NOT EXISTS staging.matches (
+  match_id SERIAL PRIMARY KEY,
   team_a varchar(40),
   team_b varchar(40),
   competition varchar(100),
@@ -8,3 +31,8 @@ CREATE TABLE IF NOT EXISTS hltv.hltv_metadata (
   match_url varchar,
   demo_url text
 );
+
+CREATE TABLE IF NOT EXISTS staging.players (
+  player_id smallint PRIMARY KEY,
+  team_id smallint FOREIGN KEY,
+)
